915d48210c6872de3ad51f926a5429c9c50148b5
CaioBrito09 <caio.debrito@dcomp.ufs.br>
2024-09-12 18:22:54 -0300

    Add files via upload

--- arrayDe5.js
+++ arrayDe5.js
@@ -1,6 +1,18 @@
-const lista = [100, 80, 60, 120, 90]
-const soma= (acc,num) => acc + num
-const multi1 = (num) => num*1
+//jeito anterior
+
+/*const lista = [100,80,60,120,90]
+onst soma = (acc,num) => acc + num 
+const soma0 = (num) => num*1
 const resultado = [(lista.reduce(soma))]
 
-console.log(resultado.map(multi1))
+console.log(resultado.map(soma0))
+*/
+
+//novo jeito
+
+const lista = [100,80,60,120,90]
+const soma = (acc,num) => acc + num 
+const soma0 = (num) => num*0 +resultado
+const resultado = (lista.reduce(soma))
+
+console.log(lista.map(soma0))
